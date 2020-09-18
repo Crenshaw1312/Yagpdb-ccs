3a7f166e63ed2b3b064b8e4d1cf2f5c053790a90
Crenshaw7860 <68561594+Crenshaw7860@users.noreply.github.com>
2020-09-18 11:00:28 -0700

    Create avatar.go

---
+++ Utility/avatar.go
@@ -0,0 +1,16 @@
+{{/*Made by Crenshaw#7860
+	Based/Learned from jo3l"s*/}}
+{{$user := .User}}
+{{$args := parseArgs 0 "`-avatar <user>`"
+(carg "userid" "user")}}
+
+{{if $args.IsSet 0}} {{/*based /learned off Jo3l's*/}}
+	{{$user = userArg ($args.Get 0)}}
+{{end}}
+
+{{sendMessage nil (cembed
+	"title" (joinStr "" (printf "%s" $user.String))
+	"image" (sdict "url" ($user.AvatarURL "128"))
+	"color" 123
+    "footer" (sdict "text" (joinStr "" "Requested by " .User.String))
+)}}
