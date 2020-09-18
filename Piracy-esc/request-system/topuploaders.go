7a13a4cde5eef5d1bea0a959151cb06cb8971eaa
Crenshaw <68561594+Crenshaw7860@users.noreply.github.com>
2020-09-18 12:25:56 -0700

    Create topuploaders.go

---
+++ Piracy-esc/request-system/topuploaders.go
@@ -0,0 +1,7 @@
+{{/*Made by Crenshaw#7860
+Trigger type: Minute Interval (5 minutes)
+This command is an optional addition, but this is the main thing that displays
+that the more you contribute and upload, the better guild member you are!*/}}
+
+{{execAdmin "clean" "100"}}
+{{execAdmin "toprep"}}
