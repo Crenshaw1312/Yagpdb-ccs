85a70ddefc58ceba64aace30030833ff189d7609
Crenshaw <68561594+Crenshaw7860@users.noreply.github.com>
2020-09-20 16:13:52 -0700

    Bugs
    Playing yourself
    Misnamed files
    Usernames don't line up after playing

--- Fun/tic-tac-toe/game start.go
+++ Fun/tic-tac-toe/game start.go
@@ -21,15 +21,20 @@ I made this from inspiration of Specky Bot and my friend Haley.*/}}
 	{{$args := parseArgs 1 "You need to ping your challenger!\n`-ttt @user`"
 (carg "userid" "player")}}
 
-	{{/*Start game?*/}}
+{{/*Start game?*/}}
 	{{if ($Games)}}
-	{{/*active ttt channel*/}}
+{{/*active ttt channel*/}}
 	     There is an already active game in this channel, try a different channel.
-	{{else}}
-	{{/*Creating a game*/}}
-		{{$id:=sendMessageRetID nil (joinStr "" "\n **Player 1:** " (userArg ($args.Get 0)).Mention  " **-** X **Player 2:**" .User.Mention " **-** O\n```\n 1 | 2 | 3\n-----------\n 4 | 5 | 6\n-----------\n 7 | 8 | 9 \n```\nRemember to do `-ttt end` when the game is over")}}
+	{{else if and (not (userArg ($args.Get 0)).Bot) (ne (toString (userArg ($args.Get 0)).ID) (toString .User.ID))}}
+{{/*Creating a game*/}}
+		{{sendMessage nil (joinStr "" "\n **Player 1:** " (userArg ($args.Get 0)).Mention  " **-** X **Player 2:**" .User.Mention " **-** O")}}
+		{{$id:=sendMessageRetID nil (joinStr "" "\n```\n 1 | 2 | 3\n-----------\n 4 | 5 | 6\n-----------\n 7 | 8 | 9 \n```\nRemember to do `-ttt end` when the game is over")}}
 		{{addMessageReactions nil $id ":one:" ":two:" ":three:" ":four:" ":five:" ":six:" ":seven:" ":eight:" ":nine:"}}
 		{{dbSet 336 (joinStr "" .Channel.ID "ttt") (joinStr "" "--" ((userArg ($args.Get 0)).ID) "><" .User.ID "><" $id "><" .Channel.ID "--")}}
-		{{dbSet 336 (joinStr "" .Channel.ID "XorO") "X"}}
+		{{dbSet 336 (joinStr "" .Channel.ID "XorO") "X"}}/
+	{{else if eq (toString (userArg ($args.Get 0)).ID) (toString .User.ID)}}
+		You cannot play yourself, go make a new friend to join you!
+	{{else if (userArg ($args.Get 0)).Bot}}
+		You can't play a bot, that's considered sad.
 	{{end}}
 {{end}}
