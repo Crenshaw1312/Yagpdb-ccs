11817426ed0ba1db2a2761196a801a9f060549c3
Crenshaw <68561594+Crenshaw7860@users.noreply.github.com>
2020-09-22 16:18:12 -0700

    Got rid of database
    If you need the data space for othe DBs do `{{dbDel 0 "width"}} and {{dbDel 0 "height"}}`

--- Fun/Picture.go
+++ Fun/Picture.go
@@ -1,48 +1,46 @@
 {{/*Made by Crenshaw#7860,
-	Regex: ^-pic(ture)?(\s(random|id(\s)?(\d){0,3}))?(\s)?((\d){1,3}x(\d){1,3})?$
-	I hope you enjoy, I plan on updating this ocassionally*/}}
-	 
-	{{$base:="https://picsum.photos/"}}
-	 
-	{{if reFind `((\d){1,3}x(\d){1,3})` .Cmd}}
-		 {{$wh:=reReplace `^-pic(ture)?(\s(random|id(\s)?(\d){0,3}))?(\s)?` .Cmd ""}}
-		 {{$wh:=split $wh "x"}}
-		 {{dbSet 0 "width" (index $wh 0)}}
-		 {{dbSet 0 "height" (index $wh 1)}}
-	{{else}}
-		 {{dbSet 0 "width" "250"}}
-		 {{dbSet 0 "height" "250"}}
-	{{end}}
-	 
-	{{if reFind "id" .Cmd}}
-		 {{$id:=reReplace `^-pic(ture)?\sid(\s)?` .Cmd ""}}
-		 {{$id:=reReplace `(\s)?((\d){1,3}x(\d){1,3})` $id ""}}
-		 {{if eq $id ""}}
-			  {{$id:= randInt 0 999}}
-			  {{$image:=(joinStr "" $base "id/" $id "/" (dbGet 0 "width").Value "/" (dbGet 0 "height").Value)}}
-			  {{sendMessage nil (cembed 
-			  "title" (toString $id)
-			  "image" (sdict "url" $image)
-			  "color" 123
-			  "timestamp" currentTime
-			  "footer" (sdict "text" (joinStr "" "From " .User.String) "icon_url" (.User.AvatarURL "256")))}}
-		 {{else}}
-			  {{$image:=(joinStr "" $base "id/" $id "/" (dbGet 0 "width").Value "/" (dbGet 0 "height").Value)}}
-			  {{sendMessage nil (cembed 
-			  "title" (toString $id)
-			  "image" (sdict "url" $image)
-			  "color" 123
-			  "timestamp" currentTime
-			  "footer" (sdict "text" (joinStr "" "From " .User.String) "icon_url" (.User.AvatarURL "256")))}}
-		 {{end}}
-	{{else if reFind "random" .Cmd}}
-		 {{$image:=(joinStr "" $base (dbGet 0 "width").Value "/" (dbGet 0 "height").Value "?random=" (randInt 0 1000000000))}}
-			  {{sendMessage nil (cembed 
-			  "title" (title "Random")
-			  "image" (sdict "url" $image)
-			  "color" 123
-			  "timestamp" currentTime
-			  "footer" (sdict "text" (joinStr "" "From " .User.String) "icon_url" (.User.AvatarURL "256")))}}
-	{{else if reFind `^-pic(ture)?$` .Cmd}}
-		 {{sendMessage nil "To get a specific picture: `-pic id<picture id>`\n     If you place nothing after `id` then it will produce a random image that has an id, unlike using `random`\nTo get a random image: `-pic random`\n     When using `random` you won't be able to get the id of the image, `random` has a wider scope of possible images though\nBy adding `<number>x<number>` to the end you can effectivley declare the` widthxheight` of the image you want\n**Examples**\n`-pic id237 500x500`\n`-pic id`\n`-pic id237`\n`-pic random`"}}
-	{{end}}
+Regex: ^-pic(ture)?(\s(random|id(\s)?(\d){0,3}))?(\s)?((\d){1,3}x(\d){1,3})?$
+I hope you enjoy, I plan on updating this ocassionally*/}}
+
+{{$base:="https://picsum.photos/"}}
+{{$width:="250"}}
+{{$height:="250"}}
+{{if reFind `((\d){1,3}x(\d){1,3})` .Cmd}}
+     {{$wh:=reReplace `^-pic(ture)?(\s(random|id(\s)?(\d){0,3}))?(\s)?` .Cmd ""}}
+     {{$wh:=split $wh "x"}}
+     {{$width = (index $wh 0)}}
+     {{$height = (index $wh 1)}}
+{{end}}
+
+{{if reFind "id" .Cmd}}
+     {{$id:=reReplace `^-pic(ture)?\sid(\s)?` .Cmd ""}}
+     {{$id:=reReplace `(\s)?((\d){1,3}x(\d){1,3})` $id ""}}
+     {{if eq $id ""}}
+          {{$id:= randInt 0 999}}
+          {{$image:=(joinStr "" $base "id/" $id "/" $width "/" $height)}}
+          {{sendMessage nil (cembed 
+          "title" (toString $id)
+          "image" (sdict "url" $image)
+          "color" 123
+          "timestamp" currentTime
+          "footer" (sdict "text" (joinStr "" "From " .User.String) "icon_url" (.User.AvatarURL "256")))}}
+     {{else}}
+          {{$image:=(joinStr "" $base "id/" $id "/" $width "/" $height)}}
+          {{sendMessage nil (cembed 
+          "title" (toString $id)
+          "image" (sdict "url" $image)
+          "color" 123
+          "timestamp" currentTime
+          "footer" (sdict "text" (joinStr "" "From " .User.String) "icon_url" (.User.AvatarURL "256")))}}
+     {{end}}
+{{else if reFind "random" .Cmd}}
+     {{$image:=(joinStr "" $base $width "/" $height "?random=" (randInt 0 1000000000))}}
+          {{sendMessage nil (cembed 
+          "title" (title "Random")
+          "image" (sdict "url" $image)
+          "color" 123
+          "timestamp" currentTime
+          "footer" (sdict "text" (joinStr "" "From " .User.String) "icon_url" (.User.AvatarURL "256")))}}
+{{else if reFind `^-pic(ture)?$` .Cmd}}
+     {{sendMessage nil "To get a specific picture: `-pic id<picture id>`\n     If you place nothing after `id` then it will produce a random image that has an id, unlike using `random`\nTo get a random image: `-pic random`\n     When using `random` you won't be able to get the id of the image, `random` has a wider scope of possible images though\nBy adding `<number>x<number>` to the end you can effectivley declare the` widthxheight` of the image you want\n**Examples**\n`-pic id237 500x500`\n`-pic id`\n`-pic id237`\n`-pic random`"}}
+{{end}}
