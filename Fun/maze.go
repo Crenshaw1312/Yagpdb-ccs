b57aed5f364eac9c5d99db9039a3ed9566806ecd
Crenshaw <68561594+Crenshaw7860@users.noreply.github.com>
2020-09-29 11:35:12 -0700

    Massive clean up

--- Fun/maze.go
+++ Fun/maze.go
@@ -2,26 +2,18 @@
 Based off Raffael#7777's Carl-Bot command here: https://carl.gg/t/260353
 Command type: prefix/mention
 It also shows the solution, and works with .ExecData*/}}
- 
+
+{{/*No touchy touchy*/}}
 {{$seed:=randInt 100000000 999999999}}
 {{$embed:=sdict "thumbnail" (sdict "url" (.User.AvatarURL "128")) "title" (title "maze") "description" (joinStr "" "Requested by " .User.String " -") "image" (sdict "url" nil) "color" 123}}
- 
+{{$crossings:=0}}
 {{if .CmdArgs}}
-     {{$cross:=(index .CmdArgs 0)}}
-     {{$maze:=(joinStr "" "http://maze5.de/cgi-bin/maze?sample=1&type=4&rows=12&columns=12&crossings=" (index .CmdArgs 0) "&seed=" $seed "&algorithm=backtracker&algorithm=0.5&foreground=%23ffffff&background=%2336393f&bordersize=16&cellsize=32&linewidth=2.5&format=png")}}
-    {{$embed.image.Set "url" $maze}}
-    {{$maze:= (joinStr "" $maze "&solution=true")}}
-    {{$embed.Set "description" (print $embed.description " [Solution](" $maze ")")}}
- 
+	{{$crossings = (index .CmdArgs 0)}}
 {{else if .ExecData}}
-   {{$maze:=(joinStr "" "http://maze5.de/cgi-bin/maze?sample=1&type=4&rows=12&columns=12&crossings=" .ExecData "&seed=" $seed "&algorithm=backtracker&algorithm=0.5&foreground=%23ffffff&background=%2336393f&bordersize=16&cellsize=32&linewidth=2.5&format=png")}}
-    {{$embed.image.Set "url" $maze}}
-    {{$maze:= (joinStr "" $maze "&solution=true")}}
-    {{$embed.Set "description" (print $embed.description " [Solution](" $maze ")")}}
-{{else}}
-   {{$maze:=(joinStr "" "http://maze5.de/cgi-bin/maze?sample=1&type=4&rows=12&columns=12&crossings=0&seed=" $seed "&algorithm=backtracker&algorithm=0.5&foreground=%23ffffff&background=%2336393f&bordersize=16&cellsize=32&linewidth=2.5&format=png")}}
-    {{$embed.image.Set "url" $maze}}
-    {{$maze:= (joinStr "" $maze "&solution=true")}}
-    {{$embed.Set "description" (print $embed.description " [Solution](" $maze ")")}}
+	{{$crossings = .ExecData}}}
 {{end}}
+{{$maze:=joinStr "" "http://maze5.de/cgi-bin/maze?sample=1&type=4&rows=12&columns=12&crossings=" $crossings "&seed=" $seed "&algorithm=backtracker&algorithm=0.5&foreground=%23ffffff&background=%2336393f&bordersize=16&cellsize=32&linewidth=2.5&format=png"}}
+{{$embed.image.Set "url" $maze}}
+{{$maze:= (joinStr "" $maze "&solution=true")}}
+{{$embed.Set "description" (print $embed.description " [Solution](" $maze ")")}}
 {{sendMessage nil (cembed $embed)}}
