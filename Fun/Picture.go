3833b86f65e8b63d50376be1278ea0cbd4396715
Crenshaw <68561594+Crenshaw7860@users.noreply.github.com>
2020-10-28 07:58:39 -0700

    improved regex

--- Fun/Picture.go
+++ Fun/Picture.go
@@ -1,12 +1,12 @@
 {{/*Made by Crenshaw#7860,
-	Regex: ^-pic(ture)?(\s(random|id(\s)?(\d){0,3}))?(\s)?((\d){1,3}x(\d){1,3})?$
+	Regex: \A-pic(ture)?(\s(random|id(\s)?(\d){0,3}))?(\s)?((\d){1,3}x(\d){1,3})?$
 	I hope you enjoy, I plan on updating this ocassionally*/}}
 	
 	{{$base:="https://picsum.photos/"}}
 	{{$width:="250"}}
 	{{$height:="250"}}
 	{{if reFind `((\d){1,3}x(\d){1,3})` .Cmd}}
-		 {{$wh:=reReplace `^-pic(ture)?(\s(random|id(\s)?(\d){0,3}))?(\s)?` .Cmd ""}}
+		 {{$wh:=reReplace `\A-pic(ture)?(\s(random|id(\s)?(\d){0,3}))?(\s)?` .Cmd ""}}
 		 {{$wh:=split $wh "x"}}
 		 {{$width = (index $wh 0)}}
 		 {{$height = (index $wh 1)}}
@@ -27,6 +27,6 @@
 	{{else if reFind "random" .Cmd}}
 		 {{$image:=(joinStr "" $base $width "/" $height "?random=" (randInt 0 1000000000))}}
 			  {{sendMessage nil (cembed "title" (title "Random") "image" (sdict "url" $image) "color" 123 "timestamp" currentTime "footer" (sdict "text" (joinStr "" "From " .User.String) "icon_url" (.User.AvatarURL "256")))}}
-	{{else if reFind `^-pic(ture)?$` .Cmd}}
+	{{else if reFind `\A-pic(ture)?$` .Cmd}}
 		 {{sendMessage nil "To get a specific picture: `-pic id<picture id>`\n     If you place nothing after `id` then it will produce a random image that has an id, unlike using `random`\nTo get a random image: `-pic random`\n     When using `random` you won't be able to get the id of the image, `random` has a wider scope of possible images though\nBy adding `<number>x<number>` to the end you can effectivley declare the` widthxheight` of the image you want\n**Examples**\n`-pic id237 500x500`\n`-pic id`\n`-pic id237`\n`-pic random`"}}
 	{{end}}
