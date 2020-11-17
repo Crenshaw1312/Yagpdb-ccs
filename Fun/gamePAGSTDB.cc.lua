499511582d7849f85bc68e0f58a54c55e16780d9
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2020-11-17 11:20:04 -0800

    First PAGSTDB command, game
    gets the cover of a game.

---
+++ Fun/gamePAGSTDB.cc.lua
@@ -0,0 +1,18 @@
+{{/*
+Trigger Type: Command (Prefix/Mention)
+Trigger: game
+
+Usage:
+`game <search>` getsthe cover image of a game.
+*/}}
+
+{{ $args := parseArgs 1 "" (carg "string" "game name") }}
+{{ $rsp := exec "hltb" ($args.Get 0) }}
+{{ $rsp = structToSdict $rsp }}
+{{ $gameImage := $rsp.Thumbnail.URL }}
+{{ $gameTitle := $rsp.Author.Name }}
+{{ sendMessage nil (cembed
+"title" $gameTitle
+"image" (sdict "url" $gameImage)
+"color" 0x4B0082
+)}}
