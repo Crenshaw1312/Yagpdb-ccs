d01476a895f22d782cc46e2ea63d79d96abaa96f
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2020-11-18 10:04:58 -0800

    Define version added

--- snippets/roleObjectArray.cc.lua
+++ snippets/roleObjectArray.cc.lua
@@ -1,6 +1,9 @@
 {{/*
   Gets an array of all roles a user has (as role object, not ID)
+  You only need one, use whatever best fits.
 */}}
+
+{{/* BASIC VERSION */}}
 
 {{ $all := (cslice) }}
 {{ range .Guild.Roles }}
@@ -12,3 +15,27 @@
     {{ end }}
 {{ end }}
 {{ $all }}
+
+{{/*BASIC VERSION END*/}}
+
+
+{{/* DEFINE VERSION */}}
+
+{{ define "roleArray" }}
+	{{ $all := (cslice) }}
+	{{ range .Guild.Roles }}
+		{{ $role := . }}
+		{{ range $.Member.Roles }}
+			{{ if eq $role.ID . }}
+				{{ $all = $all.Append $role }}
+			{{ end }}
+		{{ end }}
+	{{ end }}
+{{ .Set "Result" $all }}
+{{ end }}
+
+{{ $data := sdict "Guild" .Guild "Member" .Member }}
+{{ template "roleArray" $data }}
+{{ $data.Resault }}
+
+{{/*DEFINE VERSION END*/}}
