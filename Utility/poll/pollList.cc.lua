01dced87fe5018e13cd28a7788a838a4eaef1845
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2020-12-04 10:22:50 -0800

    Update pollList.cc.lua

--- Utility/poll/pollList.cc.lua
+++ Utility/poll/pollList.cc.lua
@@ -11,19 +11,18 @@ Requirements: pollCreate.cc.lua
               pollReaction.cc.lua
 */}}
 
-{{ $polls := dbTopEntries "poll|%|%" 10 0 }} {{ $display := "```\nNo Polls Found" }}
+{{ $polls := dbTopEntries "poll|%%|%%|%%|%%" 10 0 }} {{ $display := "" }}
 {{ range $i, $v := $polls }}
-	{{ $_ := split (slice .Key 5) "|" }} {{ $title := index $_ 1 }} {{ $msgID := toInt (index $_ 0) }} {{ $base := print "Title: " $title "\nMessage ID: " $msgID }}
-	{{ if eq $i 0 }} {{/*First line*/}}
-		{{ $display = print "```\n" $base }}
-	{{ else }} {{/*middle line(s)*/}}
-		{{ $display = print $display "\n\n" $base }}
-	{{ end }}
+{{ $_ := split (slice .Key 5) "|" }}
+	{{ $chanID := toInt (index $_ 0) }} {{ $msgID := toInt (index $_ 1) }} {{ $title := index $_ 2 }} {{ $type := index $_ 3 }}
+	{{ $link := print "https://discord.com/channels/" $.Guild.ID "/" $chanID "/" $msgID }}
+	{{ $base := print "`Title: " $title "\nType: " $type "\nMessage ID: `[" $msgID "](" $link ")" }}
+	{{ $display = print $display "\n\n" $base }}
 {{ end }}
- 
+
 {{ $e := sdict
 "Title" "Current Polls"
-"Description" (print $display "\n```")
+"Description" $display
 "Color" 0x4B0082
 }}
 {{ sendMessage nil (cembed $e) }}
