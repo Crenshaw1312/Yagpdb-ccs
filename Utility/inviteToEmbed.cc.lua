be99af9a8c298ece66ac683e4ae52ef6fb499705
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2020-12-07 15:03:28 -0800

    Invite To Embed
    Thanks LemmeCry#0001 for showing me yours, decided to make a "sleeker" version.

---
+++ Utility/inviteToEmbed.cc.lua
@@ -0,0 +1,29 @@
+{{/*
+Made by: Crensahw#1312
+Credit: LemmeCry#0001
+
+Trigger Type: Regex
+Trigger: \A(?:\s?)(https://)?discord\.(gg|com/invite)/[^\s]{8,10}(?:\s?)
+
+Note: Just paste the link in, no extra content
+*/}}
+
+{{ $id := sendMessageRetID nil (print .Cmd "#") }}
+{{ sleep 1 }}
+{{ $invite := structToSdict (index (getMessage nil $id).Embeds 0) }}
+
+{{ deleteMessage nil $id 0 }}
+
+{{ $name := reReplace `(Join\sthe\s|\sDiscord\sServer!)` $invite.Title "" }}
+{{ $members := reFindAll `\d+` $invite.Description }} {{ $members = print "Member Count: **" (index $members (sub (len $members) 1)) "**" }}
+
+{{ $embed := sdict
+	"Author" (sdict "icon_url" (.User.AvatarURL "256") "Name" .User.String)
+    "Thumbnail" (sdict "url" $invite.Thumbnail.URL)
+	"Title" $name
+  	"URL" .Cmd
+  	"Description" $members
+  	"Color" 0x4B0082
+}}
+{{ sendMessage nil (cembed $embed) }}
+{{ deleteTrigger 0 }}
