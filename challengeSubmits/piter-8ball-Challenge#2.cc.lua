bd86b003a2808dcc723f8490ffcd966e644e2b09
DaviiD1337 <63918817+DaviiD1337@users.noreply.github.com>
2020-12-27 18:07:30 +0200

    Update piter-8ball-Challenge#2.cc.lua

--- challengeSubmits/piter-8ball-Challenge#2.cc.lua
+++ challengeSubmits/piter-8ball-Challenge#2.cc.lua
@@ -1,13 +1,19 @@
+{{/*
+    Trigger: Command with trigger "-8ball"
+    
+    Make sure the in-built command is disabled (via command overrides)
+    
+*/}}
 {{/* CONFIG */}}
 {{$responses := cslice "Yes." "No." "Maybe." "I'm not sure." "Probably." "I don't know." "I don't think so."}}
 
 {{/* CODE */}}
-{{$random := (index $responses (randInt (len $responses)))}}
+{{$random := (index (shuffle $responses) 0)}}
 
 {{$embed := sdict
   "author" (sdict "name" (printf "%s (%d)" .User.String .User.ID) "icon_url" (.User.AvatarURL "256"))
   "fields" (cslice 
-      (sdict "name" "Question:" "value" (printf "%s" (reReplace `\A-8ball\s*\z` .Message.Content "")))
+      (sdict "name" "Question:" "value" (printf "%s" .StrippedMsg))
       (sdict "name" "Answer:" "value" $random)
   )
 }}
