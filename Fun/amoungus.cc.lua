7540d929b2b50267510615ccf8891558b84d9715
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2021-01-08 12:06:32 -0800

    Create amoungus.cc.lua

---
+++ Fun/amoungus.cc.lua
@@ -0,0 +1,19 @@
+{{/*
+Made by: Crensahw#1312
+
+Trigger Type: contains
+Trigger: amoung us
+*/}}
+{{ addReactions 
+":amongusvote_yellow:773940444618031124"
+":amongusvote_white:773940442621804567"
+":amongusvote_purple:773940443368914954"
+":amongusvote_orange:773940443129446461"
+":amongusvote_darkgreen:773940443762393128"
+":amongusvote_cyan:773940444437676062"
+":amongusvote_blue:773940442739769386"
+":amongusvote_black:773940443955331142"
+":amongusvote_pink:773940444567830538"
+":amongusvote_red:773940443166801920"
+":amongusvote_green:773940444081422406"
+":amongusvote_brown:773940443917975565" }}
