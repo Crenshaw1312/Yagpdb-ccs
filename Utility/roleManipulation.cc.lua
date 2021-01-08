32642298d6bacdaa1d528b4c9244447a54fe9da3
Chiranthan Bharadwaj R <chiranthan568@gmail.com>
2021-01-09 00:43:11 +0530

    Allowed both IDs and Mentions

--- Utility/roleManipulation.cc.lua
+++ Utility/roleManipulation.cc.lua
@@ -2,7 +2,7 @@
 Trigger Type: Regex
 Trigger: \A-(cl(one)?|s(wap)?|m(ove)?|st(eal)?|me(rge)?)?roles
 
-Note* chnage the `-` at the begining of the regex to your prefix
+Note* change the `-` at the begining of the regex to your prefix
 
 Usage:
 `mergeroles`/`meroles` Take all of both member's roles and give each members those roles.
@@ -11,14 +11,12 @@ Usage:
 `moveroles`/`mroles` Take the roles from the first member and give them to the second member.
 */}}
 
-{{ if eq 2 (len .Message.Mentions) }}
-	{{ parseArgs 2 "You need to ping two people, do `-roles` for help." (carg "member" "1") (carg "member" "2") }}
-	{{ $user1 := getMember (index .CmdArgs 0) }}
-	{{ $user2 := getMember (index .CmdArgs 1) }}
-	{{ $list1 := $user1.Roles }} {{ $user1 := $user1.User }}
+{{ if eq (len .CmdArgs) 2 }}
+{{ $user1 := getMember (userArg (index .CmdArgs 0)) }}
+{{ $user2 := getMember (userArg (index .CmdArgs 1)) }}
+    {{ $list1 := $user1.Roles }} {{ $user1 := $user1.User }}
 	{{ $list2 := $user2.Roles }} {{ $user2 := $user2.User}}
 	{{- $cmd := reReplace `\A-` .Cmd "" -}}
-
 	{{ if eq $cmd "cloneroles" "clroles" }}
 		{{ range $list2 }}
 			{{ takeRoleID $user2.ID . 0 }}
@@ -27,7 +25,6 @@ Usage:
 			{{ giveRoleID $user2.ID . }} 
 		{{ end }}
 		Successfully cloned the roles from **{{ $user1.Username }}** to **{{ $user2.Username }}**
-
 	{{ else if eq $cmd "swaproles" "sroles" }}
 		{{ range $list2 }}
 			{{ takeRoleID $user2.ID . 0 }}
@@ -38,19 +35,16 @@ Usage:
 			{{ takeRoleID $user1.ID . 0 }}
 		{{ end }}
 		Successfully swapped the roles between **{{ $user1.Username }}** and **{{ $user2.Username }}**
-
 	{{ else if eq $cmd "moveroles" "mroles" }}
 		{{ range $list1 }}
 			{{ giveRoleID $user2.ID . }} 
 		{{ end }}
 		Successfully moved/added the roles from **{{ $user1.Username }}** to **{{ $user2.Username }}**
-
 	{{ else if eq $cmd "stealroles" "stroles" }}
 		{{ range $list1 }}
 			{{ takeRoleID $user2.ID . }}
 		{{ end }}
 		Successfully stole/took the roles on **{{ $user1.Username }}** from **{{ $user2.Username }}**
-
 	{{ else if eq $cmd "mergeroles" "meroles" }}
 		{{ range $list1 }}
 			{{ giveRoleID $user2.ID . }}
@@ -61,7 +55,6 @@ Usage:
 			{{ giveRoleID $user1.ID . }}
 		{{ end }}
  		Successfully merged the roles between **{{ $user1.Username }}** and **{{ $user2.Username }}**
-
 	{{ end }}
 {{ else }}
 {{ sendMessage nil (cembed
